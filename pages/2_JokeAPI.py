d2fbdb8ac8446ea490c8dbf5910c2aa4a8b3775e
t-cjackson <camjack702@gmail.com>
2024-08-20 10:18:07 -0400

    added jokeapi url

--- pages/2_JokeAPI.py
+++ pages/2_JokeAPI.py
@@ -1,17 +1,32 @@
 import streamlit as st  
 from style import global_page_style2
 import time
-  
+import os
+from dotenv import load_dotenv
+import requests
+
+
+load_dotenv()
+
+
 def chat(messages, question):  
     messages.append({"role": "user", "content": "👤: " + question})  
     with st.chat_message("user", avatar="👤"):  
         st.markdown(question)  
     with st.spinner('Processing...'): 
         time.sleep(5) 
-        response = "Hi! Soon I will tell you a joke :)!"  
-        messages.append({"role": "assistant", "content": "🤖: " + response})  
-        with st.chat_message("assistant", avatar="🤖"):  
-            st.markdown(response)  
+        m_strVaultURL = os.environ["vaulturl"] + question
+        response = requests.get(m_strVaultURL)  
+        if response.status_code == 200:
+            data = response.json()
+            joke = data.get('response')
+            messages.append({"role": "assistant", "content": "🤖: " + joke})  
+            with st.chat_message("assistant", avatar="🤖"):  
+                st.markdown(joke)  
+        else:  
+            messages.append({"role": "assistant", "content": "🤖: " + f"Error: {response.status_code} - {response.text}"})  
+            with st.chat_message("assistant", avatar="🤖"):  
+                st.markdown( f"Error: {response.status_code} - {response.text}")  
   
 def clear_session(messages):  
     # Clear necessary session state variables  
